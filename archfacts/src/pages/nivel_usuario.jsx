49171758d9e5d35d713f68fc4892074d3d698f75
diegodecampos <diego.cfs.arruda@gmail.com>
2024-09-24 20:27:26 -0300

    style: adding pictures to the cards in the user level page

--- archfacts/src/pages/nivel_usuario.jsx
+++ archfacts/src/pages/nivel_usuario.jsx
@@ -8,27 +8,29 @@ function NivelUsuario() {
       <SimpleHeader />
       <div className="card-container">
         <a href="/cadastrar-beneficiario">
-        <div className="card">
-          <button className="btn">SOU UM BENEFICIÁRIO</button>
-        </div>
+          <div className="card">
+            <img src='../public/assets/imgs/beneficiario.png' alt='Beneficiário' className="card-image" />
+            <button className="btn">SOU UM BENEFICIÁRIO</button>
+          </div>
         </a>
         <a href="/cadastrar">
-        <div className="card">
-          <button className="btn">SOU UM PRESTADOR DE SERVIÇO</button>
-        </div>
+          <div className="card">
+            <img src='../public/assets/imgs/prestador.png' alt='Prestador de Serviço' className="card-image" />
+            <button className="btn">SOU UM PRESTADOR DE SERVIÇO</button>
+          </div>
         </a>
         <a href="/cadastrar">
-        <div className="card">
-          <button className="btn">SOU UM FUNCIONÁRIO</button>
-        </div>
+          <div className="card">
+            <img src='../public/assets/imgs/funcionario.png' alt='Funcionário' className="card-image" />
+            <button className="btn">SOU UM FUNCIONÁRIO</button>
+          </div>
         </a>
       </div>
       <div className='div_footer'>
-            <footer>
-              <p className='texto_footer'>
-            © 2024 ArchFacts all rights reserved.</p>
-            </footer>
-        </div>
+        <footer>
+          <p className='texto_footer'>© 2024 ArchFacts all rights reserved.</p>
+        </footer>
+      </div>
     </div>
   );
 }
