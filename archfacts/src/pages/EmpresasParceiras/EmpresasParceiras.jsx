af909e5270d71521780ac8789b966eb746c21044
GiovaneCardona <giovane.cardona@trackage.com.br>
2024-10-20 17:28:54 -0300

    Feat: Making the sidebar component and finishing empresas parceiras screen

--- archfacts/src/pages/EmpresasParceiras/EmpresasParceiras.jsx
+++ archfacts/src/pages/EmpresasParceiras/EmpresasParceiras.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import EnterpriseCard from '../../components/Enterprise-Card/enterpriseCard';
 import styles from './EmpresasParceiras.module.css'
+import SideBar from '../../components/Side-Bar/sideBar';
 
 
 const EmpresasParceiras = () => {
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.sideBar}>
 
-                </div>
+                <SideBar/>
                 <div className={styles.content}>
+                    <div className={styles.capsula}>
+                        <span className={styles.text}>Seja bem-vindo!</span>
+                        <div className={styles.welcome}>
+                        </div>
+                    </div>
+
                     <div className={styles.empresasContainer}>
                         <div className={styles.barraSuperior}>
                             <p>Prestadores contratados:</p>
