aad9a04e7c0f7afd55baa308a8469b08b573c496
GustavoS-SCruz <gustavo.cruz@sptech.school>
2024-11-18 17:19:35 -0300

    Feat: new sidebar colaborador

---
+++ archfacts/src/components/Side-Bar-Colaborador/sideBarColaborador.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./sideBarColaborador.module.css"
+import person from '../../utils/assets/person.png'
+import clipboard from '../../utils/assets/clipboard.png'
+import house from '../../utils/assets/house.png'
+
+const SideBarColaborador = () => {
+    return (
+        <div className={styles.sideBar}>
+        <img className={styles.img} style={{width: '60px', height: '60px'}} src={person} alt="" />
+        <img className={styles.img} style={{width: '60px', height: '60px'}} src={house} alt="" />
+        <img className={styles.img} style={{width: '60px', height: '60px'}} src={clipboard} alt="" />
+        </div>
+    )
+}
+export default SideBarColaborador;
