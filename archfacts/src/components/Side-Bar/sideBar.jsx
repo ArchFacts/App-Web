2d9a8f2a7b6af4fe710b442da4f10e1bffc83fd1
GiovaneCardona <giovanecardona@sptech.school>
2024-11-20 18:07:21 -0300

    Fix: Fixing sidebar icons

--- archfacts/src/components/Side-Bar/sideBar.jsx
+++ archfacts/src/components/Side-Bar/sideBar.jsx
@@ -3,13 +3,17 @@ import styles from "./sideBar.module.css"
 import person from '../../utils/assets/person.png'
 import clipboard from '../../utils/assets/clipboard.png'
 import house from '../../utils/assets/house.png'
+import aperto from '../../utils/assets/aperto.png'
 
 const SideBar = () => {
     return (
         <div className={styles.sideBar}>
-        <img className={styles.img} style={{width: '60px', height: '60px'}} src={house} alt="" />
-        <img className={styles.img} style={{width: '60px', height: '60px'}} src={clipboard} alt="" />
-        <img className={styles.img} style={{width: '60px', height: '60px'}} src={person} alt="" />
+        <img className={styles.img} style={{width: '50px', height: '50px'}} src={house} alt="" />
+        <img className={styles.img} style={{width: '50px', height: '50px'}} src={aperto} alt="" />
+        <img className={styles.img} style={{width: '50px', height: '50px'}} src={clipboard} alt="" />
+        <img className={styles.img} style={{width: '50px', height: '50px'}} src={person} alt="" />
+        
+
         </div>
 
     )
