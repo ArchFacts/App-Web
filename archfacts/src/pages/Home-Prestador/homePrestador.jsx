51d71aef5f616c33eec6c5abbf3133d9ead94c14
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2024-11-22 03:09:05 -0300

    Finishing proposta modal

--- archfacts/src/pages/Home-Prestador/homePrestador.jsx
+++ archfacts/src/pages/Home-Prestador/homePrestador.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 import styles from './homePrestador.module.css'
 import SideBarColaborador from '../../components/Side-Bar-Colaborador/sideBarColaborador';
 import EnterpriseScore from '../../components/Enterprise-Score/enterpriseScore';
 import OpenProposal from '../../components/Open-Proposal/openProposal';
+import fechar_icon from "../../utils/assets/modal-x.svg"
+
 const HomePrestador = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [propostaSelecionada, setPropostaSelecionada] = useState(null);
+
+    const abrirModal = (propostaDaVez) => {
+        setPropostaSelecionada(propostaDaVez);
+        setModalIsOpen(true);
+    }
+
+    const fecharModal = () => {
+        setModalIsOpen(false);
+        setPropostaSelecionada(null);
+    }
+
+    const propostas = [
+        {
+            solicitante: "Julia Campioto",
+            servicos: "Carros estéticos, Carros esportivos",
+            email: "julia.campioto@exemplo.com",
+            descricao: "Serviço de manutenção de carros"
+        }
+    ]
+
 
     return (
         <>
@@ -31,14 +56,51 @@ const HomePrestador = () => {
                                 <span><p>Download</p></span>
                                 <span><p>Aceitar Recusar</p></span>
                             </div>
-                            <OpenProposal
-                                solicitante={"Julia Campioto"}
-                                servicos={"Carros estéticos, Carros esportivos"}
-                            />
-                            <OpenProposal
-                                solicitante={"Julia Campioto"}
-                                servicos={"Carros estéticos, Carros clássicos, Carros esportivos "}
-                            />
+                            {propostas.map((proposta, index) => (
+                                <OpenProposal
+                                    key={index}
+                                    email={proposta.email}
+                                    solicitante={proposta.solicitante}
+                                    servicos={proposta.servicos}
+                                    onClick={() => abrirModal(proposta)}
+                                />
+                            ))}
+
+                            <Modal
+                                isOpen={modalIsOpen}
+                                onRequestClose={fecharModal}
+                                contentLabel="Modal para finalizar o projeto"
+                                className={styles.modal}
+                                overlayClassName={styles.modal_overlay}>
+
+                                <div className={styles.modal_header}>
+                                    <h2>Proposta de {propostaSelecionada.solicitante}</h2>
+                                    <img src={fechar_icon}
+                                        alt="Fechar"
+                                        onClick={fecharModal} />
+                                </div>
+
+                                {propostaSelecionada && ( // Estrutura condicional, ele só carregará o que está abaixo se tiver algo (previne erros)
+                                    <div className={styles.modal_content}>
+                                        <div className={styles.field}>
+                                            <p className={styles.title}>E-mail do solicitante:</p>
+                                            <p className={styles.content}>{propostaSelecionada.email}</p>
+                                        </div>
+                                        <div className={styles.field}>
+                                            <p className={styles.title}>Serviços escolhidos:</p>
+                                            <p className={styles.content}>{propostaSelecionada.servicos}</p>
+                                        </div>
+                                        <div className={styles.field}>
+                                            <p className={styles.title}>Descrição adicional:</p>
+                                            <p className={styles.content}>{propostaSelecionada.descricao}</p>
+                                        </div>
+                                        <div className={styles.button_area}>
+                                            <div className={styles.button}><p>Recusar proposta</p></div>
+                                            <div className={styles.button}><p>Aceitar Proposta/</p></div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Modal>
                         </div>
                     </div>
                 </div>
