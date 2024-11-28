b415c28e1095da670fc5d435e067d653114d033f
diegodecampos <diego.cfs.arruda@gmail.com>
2024-11-28 17:20:08 -0300

    Feat: Adding accessibility to the website

--- archfacts/src/main.jsx
+++ archfacts/src/main.jsx
@@ -4,8 +4,18 @@ import App from './App.jsx';
 import './utils/global.css';
 import '../index.html';
 
+const initializeVLibras = () => {
+  if (window.VLibras) {
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  } else {
+    console.error('VLibras não carregado');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', initializeVLibras);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
