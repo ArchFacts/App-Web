a8b2587e1abdc5a19d6569868cd9fe969f4d5d6e
juliaacampioto <julia.campioto@sptech.school>
2024-11-29 00:31:16 -0300

    feat: finalize create activity modal

--- archfacts/src/components/Abrir-Tarefa/abrir_tarefa.jsx
+++ archfacts/src/components/Abrir-Tarefa/abrir_tarefa.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import styles from '../Abrir-Tarefa/abrir_tarefa.module.css'
 import adicionar_icon from "../../utils/assets/plus_signal.svg"
 
-
 const AbrirTarefa = ({ onAbrirTarefaClick }) => {
     return (
         <div className={styles.abrir_tarefa} onClick={onAbrirTarefaClick}>
-            <h1>Abrir Tarefa</h1>
-            <img src={adicionar_icon}
-                alt="icone de adicionar"
-                width={60}
-                height={50} />
-        </div >
-    )
-}
+            <h1>Criar tarefa</h1>
+            <img src={adicionar_icon} alt="icone de adicionar" width={60} height={50} />
+        </div>
+    );
+};
 
 export default AbrirTarefa;
